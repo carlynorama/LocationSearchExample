a3ad8943195e62ac4227ce9b74a62d11ee6b7754
carlynorama <carlynorama@gmail.com>
2022-08-12 16:12:55 -0700

    Initial Commit

---
+++ LocationSearchResults/LocationSearchResultsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationSearchResultsApp.swift
+//  LocationSearchResults
+//
+//  Created by Labtanza on 8/12/22.
+//
+
+import SwiftUI
+
+@main
+struct LocationSearchResultsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
